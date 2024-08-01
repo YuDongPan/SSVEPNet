e53c5112dbc43687394ccb95727d8b4f6de095c6
Yudong Pan <panydacademy@163.com>
2024-08-01 14:49:07 +0800

    Update LossFunction.py
    Speed Up the Calculation of Smoothed Labels

--- Utils/LossFunction.py
+++ Utils/LossFunction.py
@@ -1,6 +1,7 @@
 # Designer:Pan YuDong
 # Coder:God's hand
 # Time:2022/3/29 19:17
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -38,30 +39,35 @@ class CELoss_Marginal_Smooth(nn.Module):
                           int(0 <= (i // self.cols + 1) <= self.rows - 1 and 0 <= i % self.cols + 1 <= self.cols - 1))
                                for j in range(class_num)] for i in range(class_num)]
 
+        self.attention_lst = np.asarray(self.attention_lst)
 
-        # print("att_lst:", self.attention_lst)
 
     def forward(self, outputs, targets):
         '''
-        :param outputs: predictive results,shape:(batch_size, class_num)
-        :param targets: ground truth,shape:(batch_size, )
+        :param outputs: predictive results, shape: (batch_size, class_num)
+        :param targets: ground truth, shape: (batch_size,)
         :return:
         '''
+        batch_size, class_num = outputs.shape
+
+        # Obtain target labels and create smooth labels
         targets_data = targets.cpu().data
-        smoothed_labels = torch.empty(size=(outputs.shape[0], outputs.shape[1]))
+        smoothed_labels = torch.zeros(size=(batch_size, class_num), device=outputs.device)
+
+        # Fill in the remaining parts of the attention matrix
         for i in range(smoothed_labels.shape[0]):
             label = targets_data[i]
-            for j in range(smoothed_labels.shape[1]):
-                if j == label:
-                    smoothed_labels[i][j] = 1.0
-                else:
-                    smoothed_labels[i][j] = self.attention_lst[label][j]
-        smoothed_labels = smoothed_labels.to(outputs.device)
+            smoothed_labels[i] = torch.from_numpy(self.attention_lst[label])
 
+        # Fill the smooth label with a position assignment of 1.0
+        smoothed_labels[torch.arange(batch_size), targets_data] = 1.0
+
+        # Calculate loss
         log_prob = F.log_softmax(outputs, dim=1)
-        att_loss = - torch.sum(log_prob * smoothed_labels) / outputs.size(-2)
+        att_loss = -torch.sum(log_prob * smoothed_labels) / batch_size
         ce_loss = nn.CrossEntropyLoss()(outputs, targets)
         loss_add = self.alpha * ce_loss + (1 - self.alpha) * att_loss
+
         return loss_add
 
 
